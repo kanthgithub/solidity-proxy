5963455721e95e7112ba4d3588bb5e646e22c024
lakshmi kanth <kanth.explorer@gmail.com>
2019-11-26 15:30:10 +0800

    Update ArithmeticProxy.sol

--- contracts/ArithmeticProxy.sol
+++ contracts/ArithmeticProxy.sol
@@ -12,6 +12,15 @@ contract ArithmeticProxy{
         require(success, "Construction failed");
     }
 
+    function updateContractLogic(bytes memory constructData, address contractLogic) public {
+        // save the code address
+        assembly { // solium-disable-line
+            sstore(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7, contractLogic)
+        }
+        (bool success, /* bytes memory _*/ ) = contractLogic.delegatecall(constructData); // solium-disable-line
+        require(success, "Construction failed");
+    }
+
     function() external payable {
         assembly { // solium-disable-line
             let contractLogic := sload(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7)
