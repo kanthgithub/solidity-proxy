f1c8c13f3ce317f097e9f1c3649f17c627dd5596
lakshmikanth <kanth.explorer@gmail.com>
2019-11-30 14:49:25 +0800

    Proxy with multiple logic contracts

--- contracts/ProxyV2.sol
+++ contracts/ProxyV2.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.5.1;
 
 contract ProxyV2{
 
+    address owner = msg.sender;
+
     // Code position in storage is keccak256("PROXIABLE") = "0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7"
     // Code position in storage is keccak256("PROXIABLEV2") = "0x999802572af9bd298e7e25fe3959ac083d4486b31ed6b1314afdaea9eb9e0def"
     constructor(bytes memory constructData1, address contractLogic1,bytes memory constructData2, address contractLogic2) public {
@@ -19,6 +21,8 @@ contract ProxyV2{
     }
 
     function updateContractLogic1(bytes memory constructData, address contractLogic) public {
+        require(msg.sender == owner);
+
         // save the code address
         assembly { // solium-disable-line
             sstore(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7, contractLogic)
@@ -28,6 +32,8 @@ contract ProxyV2{
     }
 
         function updateContractLogic2(bytes memory constructData, address contractLogic) public {
+        require(msg.sender == owner);
+        
         // save the code address
         assembly { // solium-disable-line
             sstore(0x999802572af9bd298e7e25fe3959ac083d4486b31ed6b1314afdaea9eb9e0def, contractLogic)
@@ -36,27 +42,20 @@ contract ProxyV2{
         require(success, "Construction failed");
     }
 
-    function arithmetic() external payable {
+    function execute(bytes32 contractName, bytes32 functionName, bytes calldata data) external payable {
         assembly { // solium-disable-line
-            let contractLogic := sload(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7)
-            calldatacopy(0x0, 0x0, calldatasize)
-            let success := delegatecall(sub(gas, 10000), contractLogic, 0x0, calldatasize, 0, 0)
-            let retSz := returndatasize
-            returndatacopy(0, 0, retSz)
-            switch success
-            case 0 {
-                revert(0, retSz)
-            }
-            default {
-                return(0, retSz)
-            }
-        }
-    }
+            let contractLogic := 0
+            switch(contractName)
+                case "Arithmetic" {
+                    contractLogic := sload(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7)
+                }
+                default {
+                    contractLogic := sload(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7)
+                }
 
-    function refund() external payable {
-        assembly { // solium-disable-line
-            let contractLogic := sload(0x999802572af9bd298e7e25fe3959ac083d4486b31ed6b1314afdaea9eb9e0def)
             calldatacopy(0x0, 0x0, calldatasize)
+            //bytes4 sig = bytes4(keccak256("add(uint256,uint256)"));
+
             let success := delegatecall(sub(gas, 10000), contractLogic, 0x0, calldatasize, 0, 0)
             let retSz := returndatasize
             returndatacopy(0, 0, retSz)
