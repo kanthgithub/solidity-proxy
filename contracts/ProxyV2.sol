9cd4a717dd4d211ff09ee679bee30ef68f804b54
lakshmikanth <kanth.explorer@gmail.com>
2019-12-01 23:33:27 +0800

    Fix compilation issues in proxyV2

--- contracts/ProxyV2.sol
+++ contracts/ProxyV2.sol
@@ -21,8 +21,7 @@ contract ProxyV2{
     }
 
     function updateContractLogic1(bytes memory constructData, address contractLogic) public {
-        require(msg.sender == owner);
-
+        require(msg.sender == owner,'only owner can update contract logic');
         // save the code address
         assembly { // solium-disable-line
             sstore(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7, contractLogic)
@@ -32,8 +31,7 @@ contract ProxyV2{
     }
 
         function updateContractLogic2(bytes memory constructData, address contractLogic) public {
-        require(msg.sender == owner);
-        
+        require(msg.sender == owner,'only owner can update contract logic');
         // save the code address
         assembly { // solium-disable-line
             sstore(0x999802572af9bd298e7e25fe3959ac083d4486b31ed6b1314afdaea9eb9e0def, contractLogic)
@@ -53,7 +51,7 @@ contract ProxyV2{
                      switch functionName
                             case "add"  {
                                 functionHash := keccak256("add(uint256,uint256)",4)
-                            }    
+                            }
                             case "substract"  {
                                 functionHash := keccak256("substract(uint256,uint256)",4)
                             }
@@ -64,10 +62,10 @@ contract ProxyV2{
 
                  case "RefundEther"  {
                     contractLogic := sload(0x999802572af9bd298e7e25fe3959ac083d4486b31ed6b1314afdaea9eb9e0def)
-                     switch functionName 
+                     switch functionName
                              case "loopbackEther" {
                                 functionHash := keccak256("loopbackEther()",4)
-                            }    
+                            }
                            default {
                                 functionHash := keccak256("loopbackEther()",4)
                             }
@@ -78,12 +76,11 @@ contract ProxyV2{
                         switch functionName
                             case "loopbackEther" {
                                 functionHash := keccak256("loopbackEther()",4)
-                            }    
+                            }
                            default {
                                 functionHash := keccak256("loopbackEther()",4)
                             }
                 }
-        
             calldatacopy(0x0, 0x0, calldatasize)
             let success := delegatecall(sub(gas, 10000), contractLogic, 0x0, calldatasize, 0, 0)
             let retSz := returndatasize
