0e35e660f51ddb5172c876a40da87d4b4bfcc354
lakshmikanth <kanth.explorer@gmail.com>
2019-12-01 21:49:06 +0800

    Fix compilation issues in proxyV2

--- contracts/ProxyV2.sol
+++ contracts/ProxyV2.sol
@@ -43,57 +43,51 @@ contract ProxyV2{
     }
 
     function execute(bytes32 contractName, bytes32 functionName, bytes calldata data) external payable {
+        
+        bytes4 functionHash = 0;
+        bytes4 sig = 0;
+
         assembly { // solium-disable-line
             let contractLogic := 0
-            bytes4 functionHash := 0
-            switch(contractName)
-                case "Arithmetic" {
+            switch contractName
+                case "Arithmetic"  {
                     contractLogic := sload(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7)
-                     switch(functionName)
-                            case "add" {
-                                functionHash = bytes4(keccak256("add(uint256,uint256)"))
+                     switch functionName
+                            case "add"  {
+                                functionHash := keccak256("add(uint256,uint256)",4)
                             }    
-                            case "substract" {
-                                functionHash = bytes4(keccak256("substract(uint256,uint256)"))
+                            case "substract"  {
+                                functionHash := keccak256("substract(uint256,uint256)",4)
                             }
-                            default {
-                               functionHash = bytes4(keccak256("add(uint256,uint256)"))
+                            default  {
+                               functionHash := keccak256("add(uint256,uint256)",4)
                             }
                 }
 
-                 case "RefundEther" {
+                 case "RefundEther"  {
                     contractLogic := sload(0x999802572af9bd298e7e25fe3959ac083d4486b31ed6b1314afdaea9eb9e0def)
-                     switch(functionName)
+                     switch functionName 
                              case "loopbackEther" {
-                                functionHash = bytes4(keccak256("loopbackEther()"))
+                                functionHash := keccak256("loopbackEther()",4)
                             }    
                            default {
-                                functionHash = bytes4(keccak256("loopbackEther()"))
+                                functionHash := keccak256("loopbackEther()",4)
                             }
-                }
+                 }
 
                 default {
                     contractLogic := sload(0x999802572af9bd298e7e25fe3959ac083d4486b31ed6b1314afdaea9eb9e0def)
-                        switch(functionName)
+                        switch functionName
                             case "loopbackEther" {
-                                functionHash = bytes4(keccak256("loopbackEther()"))
+                                functionHash := keccak256("loopbackEther()",4)
                             }    
                            default {
-                                functionHash = bytes4(keccak256("loopbackEther()"))
+                                functionHash := keccak256("loopbackEther()",4)
                             }
                 }
-
-
-            //TODO starts-here 
-            //use calldata argument to function along with functionHash in delegateCall
-
+        
             calldatacopy(0x0, 0x0, calldatasize)
-            //bytes4 sig = bytes4(keccak256("add(uint256,uint256)"));
-
             let success := delegatecall(sub(gas, 10000), contractLogic, 0x0, calldatasize, 0, 0)
-            
-            //TODO ends here
-
             let retSz := returndatasize
             returndatacopy(0, 0, retSz)
             switch success
